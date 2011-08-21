fa88f0d09095d5c58c7e08f046ac3fd0a92a3cdd
Lucas Romano <romano.lucas@gmail.com>
2011-08-21 03:44:02 +0200

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,15 @@
 #
 
 class User < ActiveRecord::Base
-
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   attr_accessible :name, :email
-
+  
+  validates :name,  :presence => true,
+                    :length => {:maximum => 50}
+  validates :email, :presence => true,
+                    :format => {:with => email_regex},
+                    :uniqueness => { :case_sensitive => false}
+  
 end
