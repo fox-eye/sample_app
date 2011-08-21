93ad51198c28a8e959b21516d01bc0b19a0a85b2
Lucas Romano <romano.lucas@gmail.com>
2011-08-21 23:37:57 +0200

    Added password and encrypted password attributes to to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,8 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation 
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
-  attr_accessible :name, :email
   
   validates :name,  :presence => true,
                     :length => {:maximum => 50}
@@ -21,4 +10,21 @@ class User < ActiveRecord::Base
                     :format => {:with => email_regex},
                     :uniqueness => { :case_sensitive => false}
   
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40}
+  
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#
+
