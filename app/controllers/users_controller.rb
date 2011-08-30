0bff229387a98d6399bd94586b243ee78cff4b44
Lucas Romano <romano.lucas@gmail.com>
2011-08-31 01:17:56 +0200

    Can now destroy users(as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:index,:edit,:update]
+  before_filter :authenticate, :only => [:index,:edit,:update,:destroy]
   before_filter :correct_user, :only => [:edit,:update]
+  before_filter :admin_user, :only => :destroy
   
   def index
     @title = "All users"
@@ -32,17 +33,20 @@ class UsersController < ApplicationController
   
   def edit
     @title = "Edit user"
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       redirect_to @user, :flash => {:success => "Profile updated."}
     else
       @title = "Edit user"
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => {:success => "User destroyed."}
   end
   
   private
@@ -54,5 +58,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-
+    
+    def admin_user
+      user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current_user?(user)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#
-
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation 
@@ -64,3 +52,17 @@ class User < ActiveRecord::Base
    end
     
 end
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean         default(FALSE)
+#
+
