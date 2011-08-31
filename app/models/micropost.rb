7ef71b1f210715db11ac7cd8a1b96702d92335ae
Lucas Romano <romano.lucas@gmail.com>
2011-08-31 23:51:14 +0200

    Micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,10 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   
   default_scope :order => "microposts.created_at DESC"
+  
+  validates :content, :presence => true,
+                      :length => { :maximum => 140}
+  validates :user_id, :presence => true
 end
 
 # == Schema Information
