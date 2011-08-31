ff30f948d1a0948da206bae6f95ea8b3fc496123
Lucas Romano <romano.lucas@gmail.com>
2011-09-01 01:29:13 +0200

    Showing microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -111,6 +111,27 @@ describe UsersController do
                                             :content => user_path(@user),
                                             :href => user_path(@user))
     end
+    
+    it "should show the users's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar !")
+      mp2 = Factory(:micropost, :user => @user, :content => "Hourra !")
+      get :show, :id => @user
+      response.should have_selector('span.content',:content => mp1.content)
+      response.should have_selector('span.content',:content => mp2.content)
+    end
+    
+      it "should paginate microposts" do
+       35.times { Factory(:micropost, :user => @user, :content => "foo") }
+       get :show, :id => @user
+       response.should have_selector('div.pagination')
+     end
+      
+      it "should display the microposts count" do
+        11.times { Factory(:micropost, :user => @user, :content => "foo") }
+        get :show, :id => @user
+        response.should have_selector('td.sidebar',:content => @user.microposts.count.to_s)
+      end
+    
   end
   
   
